488d8d55b7d0152fd6ed60ccb9eac63da2c32686
iliassk <kenfaoui.iliass@gmail.com>
2020-02-16 15:38:24 +0100

    Handling react routing on netlify refreshing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./app/index.jsx",
@@ -17,7 +18,12 @@ module.exports = {
   plugins: [
       new HtmlWebpackPlugin({
         template: "app/index.html"
-      })
+      }),
+      new CopyWebpackPlugin([
+          {
+              from: '_redirects'
+          }
+      ])
   ],
   devServer: {
     historyApiFallback: true
